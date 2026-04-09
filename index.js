c590b47dcb418241a1fe7120d50ce6893b6cbd82
zavochias <zavoqs@gmail.com>
2026-04-08 20:18:01 -0500

    index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var toggle = true;
-
-function send_toggle() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {greeting: "toggle"});
-    });
-}
-
-window.addEventListener("load", function () {
-    try {
-        send_toggle();
-    }
-    catch (error) {
-        console.error("Error sending toggle message:", error);
-    }
-});
-
+console.log("BombParty Helper popup opened");
